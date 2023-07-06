7a951fb1d27ccc0159ce17ff8cb81749509d70d7
ilona1009 <maliarenko.ilona@gmail.com>
2023-07-06 16:36:23 +0200

    done

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,15 +2,17 @@
 // axios.defaults.headers.common["x-api-key"] = 'live_WG6vl9M9qjQ1gVgExJrrh8l7DGKY7sHJyyX4yw8ttqNDbCX0qBUyIr231rAeUOV8';
 
 const refs = {
-    select: document.querySelector('breed_selector'),
+    select: document.querySelector('.breed-select'),
     infoCat: document.querySelector('.cat-info'),
     imgCat: document.querySelector('breed_image'),
 }
 
+            let storedBreeds = [];
+
+
 function fetchBreeds() {
     const REPLACE_ME = 'live_WG6vl9M9qjQ1gVgExJrrh8l7DGKY7sHJyyX4yw8ttqNDbCX0qBUyIr231rAeUOV8';
     const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-    let storedBreeds = [];
     
     return fetch(BASE_URL, {headers: {
         'x-api-key': REPLACE_ME
@@ -21,49 +23,64 @@ function fetchBreeds() {
             }
             return resp.json()
         })
-        
 }
 
-
+let cat = "";
 
 fetchBreeds().then (cat => {
     storedBreeds = cat;
 
-
     for (let i = 0; i < storedBreeds.length; i++) {
         const breed = storedBreeds[i];
-                            // console.log(breed)
+        cat = `${breed.id}`;
+                            // console.log(cat)
         let option = document.createElement('option')
-
-        if(!breed.image)continue
-    
-        option.value = i;
+     
+        option.value = cat;
         option.innerHTML = `${breed.name}`;
         document.getElementById('breed_selector').appendChild(option);
-    }
-    
+    }  
 })
     .catch(err => console.log(err))
 
+refs.select.addEventListener('change', handlerSelect);
+function handlerSelect(e) {
+    const catBreed = e.target.value;
+    console.log(catBreed);
 
-function showBreedImage() {
+    function fetchCatByBreed(cat) {
     const REPLACE_ME = 'live_WG6vl9M9qjQ1gVgExJrrh8l7DGKY7sHJyyX4yw8ttqNDbCX0qBUyIr231rAeUOV8';
-    const BASE_URL = 'https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}';
-    let storedBreeds = [];
+    const BASE_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${cat}`;
     
-    return fetch(BASE_URL, {
-        headers: {
-            'x-api-key': REPLACE_ME
-        }
-    })
+    return fetch(BASE_URL, {headers: {
+        'x-api-key': REPLACE_ME
+    }})
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(resp.statusText)
+            }
+            return resp.json()
+        })
 }
 
-showBreedImage().then(cat => {
-        storedBreeds = cat;
-            refs.imgCat =  document.getElementById("breed_image").src = storedBreeds[index].image.url;
+fetchCatByBreed('beng').then(cat => {
+
+    storedBreeds = cat;
+})
 
 
-}
 
-)
+        // console.log(storedBreeds)
+
+    // for (let i = 0; i < storedBreeds.length; i++) {
+    //     const breed = storedBreeds[i];
+    //     let breed_ids = `${breed.id}`;
+    //                         // console.log(breed_ids)
+    //     let option = document.createElement('option')
+        
+    //     if(!breed.image)continue  
+    //     option.value = i;
+    //     option.innerHTML = `${breed.name}`;
+    //     document.getElementById('breed_selector').appendChild(option);
+    // }  
 
