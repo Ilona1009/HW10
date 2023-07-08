e4fa877a5defb50eea65fbc3b347ce3e175e5ade
ilona1009 <maliarenko.ilona@gmail.com>
2023-07-08 16:35:34 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import {fetchBreeds, fetchCatByBreed} from "./cat-api.js";
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
+
 
 const refs = {
     select: document.querySelector('.breed-select'),
     infoCat: document.querySelector('.cat-info'),
     imgCat: document.querySelector('breed_image'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+
 }
+
+// refs.loader.hidden = false;
+// refs.error.hidden = true;
+refs.infoCat.setAttribute('hidden', '')
+
 
             let storedBreeds = [];
 
-fetchBreeds().then (results => {
+fetchBreeds().then(results => {
+  // refs.loader.hidden = true;
+  refs.select.removeAttribute('hidden');
+
 refs.select.insertAdjacentHTML('beforeend', createMarkUpSelect(results))  
 new SlimSelect({
     select: '.breed-select',
   });
 })
-    .catch(err => console.log(err))
+  .catch(err => {
+      refs.select.setAttribute('hidden', '');
+      // refs.error.hidden = false;
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page');
+      console.log(err)
+    })
 
     function createMarkUpSelect(arr) {
   return arr.map(el => {
@@ -26,16 +44,28 @@ new SlimSelect({
 
 
 refs.select.addEventListener('change', handlerSelect);
+
 function handlerSelect(e) {
-    const catBreed = e.target.value;
-    console.log(catBreed);
+  refs.infoCat.setAttribute('hidden', '');
+  const catBreed = e.target.value;
+  // refs.loader.hidden = false;
+      Notiflix.Notify.info('Loading data, please wait..');
+  refs.select.setAttribute('hidden', '');
 
     fetchCatByBreed(catBreed).then(cat => {
-        // createMarkUp(cat)
-        console.log(cat)
-        refs.infoCat.innerHTML = createMarkUp(cat);
+      refs.select.removeAttribute('hidden');
+      createMarkUp(cat);
+      // refs.loader.hidden = true;
+      refs.infoCat.removeAttribute('hidden')
+      refs.infoCat.innerHTML = createMarkUp(cat);
     })
-        .catch(err => console.log(err))
+      .catch(err => {
+        refs.select.setAttribute('hidden', '');
+        // refs.error.hidden = false;
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page');
+
+        console.log(err)
+      })
 }
 
 
