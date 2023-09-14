a59bd0a77136baeeb3e4db79c704c06f0ddc9ae9
ilona1009 <maliarenko.ilona@gmail.com>
2023-09-14 11:34:30 +0200

    style done

--- src/index.js
+++ src/index.js
@@ -9,12 +9,9 @@ const refs = {
     infoCat: document.querySelector('.cat-info'),
     imgCat: document.querySelector('breed_image'),
     loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
-
+  error: document.querySelector('.error'),
 }
 
-// refs.loader.hidden = false;
-// refs.error.hidden = true;
 refs.infoCat.setAttribute('hidden', '')
 
 
@@ -48,22 +45,18 @@ refs.select.addEventListener('change', handlerSelect);
 function handlerSelect(e) {
   refs.infoCat.setAttribute('hidden', '');
   const catBreed = e.target.value;
-  // refs.loader.hidden = false;
       Notiflix.Notify.info('Loading data, please wait..');
   refs.select.setAttribute('hidden', '');
 
     fetchCatByBreed(catBreed).then(cat => {
       refs.select.removeAttribute('hidden');
       createMarkUp(cat);
-      // refs.loader.hidden = true;
       refs.infoCat.removeAttribute('hidden')
       refs.infoCat.innerHTML = createMarkUp(cat);
     })
       .catch(err => {
         refs.select.setAttribute('hidden', '');
-        // refs.error.hidden = false;
             Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page');
-
         console.log(err)
       })
 }
@@ -74,8 +67,8 @@ function createMarkUp(array) {
     const { name, description, temperament } = breeds[0];
     return `
     <img class="cat-img" src="${url}" alt="${name}" width = "300">
-    <div class="cat-descr"
-      <h2>${name}</h2>
+    <div class="cat-descr">
+      <h2 class="cat-title">${name}</h2>
       <p><span class="bold">Description:</span> ${description}</p>
       <p><span class="bold">Temperament:</span> ${temperament}</p>
     </div>
